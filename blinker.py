a9920992dfac8b032f24a47b130becfda85fd636
tuhota <96323290+tuhota@users.noreply.github.com>
2022-04-27 23:52:57 +1000

    Update blinker.py

--- blinker.py
+++ blinker.py
@@ -1,17 +1,23 @@
 import RPi.GPIO as GPIO
 import time 
 
+#define pin for output
 pin = 8
 
+#set pin numbering
 GPIO.setmode(GPIO.BOARD)
 
+#set pin to output mode
 GPIO.setup(pin, GPIO.OUT)
 
 try:
 	while 1:
+		#turn on pin, wait, turn off, wait, to create blink effect
 		GPIO.output(pin, GPIO.HIGH)
 		time.sleep(0.25)
 		GPIO.output(pin, GPIO.LOW)
 		time.sleep(0.25)
+	#thrown on interrupt signal
 except KeyboardInterrupt:
+	#reset pins to input mode
 	GPIO.cleanup()
